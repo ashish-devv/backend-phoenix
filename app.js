5ffd2ef4a93f6e3d63906a7d017af57894bf6557
ashish-devv <akashish908@gmail.com>
2021-04-13 23:36:19 +0530

    added mongoose

--- app.js
+++ app.js
@@ -14,14 +14,24 @@ mongoose.connect(
   }
 );
 
-app
-  .route("/")
-  .get((req, res) => {
-    res.json({ message: "hello world" });
-  })
-  .post((req, res) => {
-    res.json({ desc: "posted" });
+const memberSchema = mongoose.Schema({
+  name: String,
+  phone: String,
+});
+
+const member = mongoose.model("member", memberSchema);
+
+app.route("/").get((req, res) => {
+  res.json({ message: "Welcome To Phoenix 👩‍💻" });
+});
+
+app.route("/members").get((req, res) => {
+  member.find((err, mem) => {
+    if (!err) {
+      res.json(mem);
+    }
   });
+});
 
 const port = process.port || 3000;
 
